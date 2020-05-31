18dcdd7a5824c3a573453c078d227bbbc3ab588b
sergio garzon <sggarzonutn@gmail.com>
2020-05-31 15:35:40 -0300

    UI Battle un poco más cuadrada

---
+++ Assets/Scripts/Logic Script/ObjectsColision/CollisionWithObjects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionWithObjects : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Tree": break;
+            case "NoTree": break;
+            case "Arcade": break;
+            case "CollisionShop": break;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "": break;
+        }
+    }
+
+}
